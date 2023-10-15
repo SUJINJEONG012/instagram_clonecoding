d99c34806e96118cbc7f0a17c94421ed83784a61
SUJINJEONG012 <peekaboo32@naver.com>
2023-10-15 22:57:04 +0900

    UserDetailsService에 아이디,비밀번호,권한을  CustomUserDetails객체에 담아 리턴

--- src/main/java/com/instagram/config/auth/CustomUserDetailsService.java
+++ src/main/java/com/instagram/config/auth/CustomUserDetailsService.java
@@ -1,5 +1,34 @@
 package com.instagram.config.auth;
 
-public class CustomUserDetailsService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.instagram.entity.User;
+import com.instagram.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		//UserDetailsService를 상속받아 loadUserByUsername를 반드시 재정의해줘야한다
+		User user = userRepository.findByUsername(username);
+		
+		if(user == null) throw new UsernameNotFoundException(username);
+		
+		CustomUserDetails customUser = new CustomUserDetails();
+		customUser.setUsername(user.getUsername());
+		customUser.setPassword(user.getPassword());
+		customUser.setRole(user.getRole());
+		return customUser;
+	}
 
 }
