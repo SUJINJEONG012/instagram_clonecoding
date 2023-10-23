332cbb70dac33eac6b8d7724b33ff6dfd0c2b869
angela <peekaboo32@naver.com>
2023-10-23 18:04:21 +0900

    commit

--- src/main/java/com/instagram/service/ImageService.java
+++ src/main/java/com/instagram/service/ImageService.java
@@ -1,11 +1,17 @@
 package com.instagram.service;
 
 import java.awt.Image;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.instagram.config.auth.PrincipalDetails;
+import com.instagram.dto.ImageUploadDto;
 import com.instagram.repository.ImageRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,5 +26,31 @@ public class ImageService {
 	public List<Image> 인기사진(){
 		return imageRepository.mPopular();
 	}
+	
+	@Value("${file.path}")
+	private String uploadFolder;
+	
+	
+	@Transactional
+	public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+		UUID uuid = UUID.randomUUID(); // uuid
+		String imageFileName = uuid+"_"+imageUploadDto.getFile().getOriginalFilename(); // 1.jpg
+		System.out.println("이미지 파일이름 : "+imageFileName);
+		
+		Path imageFilePath = Paths.get(uploadFolder+imageFileName);
+		
+		// 통신, I/O -> 예외가 발생할 수 있다.
+		try {
+			Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// image 테이블에 저장
+		Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser()); // 5cf6237d-c404-43e5-836b-e55413ed0e49_bag.jpeg
+		imageRepository.save(image);
+	}
+	
+	
 	
 }
