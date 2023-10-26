c6f3a5c71df9ed1f4c4605ecc342d797b192b7a5
angela <peekaboo32@naver.com>
2023-10-26 18:00:56 +0900

    프로필 수정 오류

---
+++ src/main/java/com/instagram/controller/api/UserApiController.java
@@ -0,0 +1,30 @@
+package com.instagram.controller.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.instagram.config.auth.PrincipalDetails;
+import com.instagram.dto.CMRespDto;
+import com.instagram.entity.User;
+import com.instagram.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+
+	private final UserService userService;
+	
+	@PutMapping("/api/user/{principalId}/profileImageUrl")
+	public ResponseEntity<?> profileImageUrlUpdate(@PathVariable int principalId, MultipartFile profileImageFile, @AuthenticationPrincipal PrincipalDetails principalDetails){
+		User userEntity =  userService.회원프로필사진변경(principalId, profileImageFile);
+		principalDetails.setUser(userEntity); //세션 변경
+		return new ResponseEntity<>(new CMRespDto<>(1, "프로필사진 변경 성공", null), HttpStatus.OK);
+	}
+}
