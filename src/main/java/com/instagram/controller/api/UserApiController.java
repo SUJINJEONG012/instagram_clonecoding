17b49bc6b800ae0ee90797c58888c931ebdc482f
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-05 22:17:26 +0900

    commit

--- src/main/java/com/instagram/controller/api/UserApiController.java
+++ src/main/java/com/instagram/controller/api/UserApiController.java
@@ -1,11 +1,14 @@
 package com.instagram.controller.api;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.instagram.config.auth.PrincipalDetails;
 import com.instagram.dto.CMRespDto;
+import com.instagram.dto.SubscribeDto;
 import com.instagram.dto.UserUpdateDto;
 import com.instagram.entity.User;
+import com.instagram.service.SubscribeService;
 import com.instagram.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,7 +29,7 @@ import lombok.RequiredArgsConstructor;
 public class UserApiController {
 
 	private final UserService userService;
-	//private final SubscribeService subscribeServcie;
+	private final SubscribeService subscribeService;
 	
 	@PutMapping("/api/user/{principalId}/profileImageUrl")
 	public ResponseEntity<?> profileImageUrlUpdate(@PathVariable int principalId, MultipartFile profileImageFile, @AuthenticationPrincipal PrincipalDetails principalDetails){
@@ -35,6 +40,11 @@ public class UserApiController {
 		return new ResponseEntity<>(new CMRespDto<>(1, "프로필사진 변경 성공", null), HttpStatus.OK);
 	}
 	
+	@GetMapping("/api/user/{pageUserId}/subscribe")
+	public ResponseEntity<?> subscribeList(@PathVariable int pageUserId, @AuthenticationPrincipal PrincipalDetails principalDetails){
+		List<SubscribeDto> subscribeDto = subscribeService.구독리스트(principalDetails.getUser().getId(), pageUserId);
+		return new ResponseEntity<>(new CMRespDto<>(1, "구독자 정보 리스트 가져오기 성공", subscribeDto), HttpStatus.OK);
+	}
 
 	
 	@PutMapping("/api/user/{id}")
--- src/main/java/com/instagram/service/SubscribeService.java
+++ src/main/java/com/instagram/service/SubscribeService.java
@@ -1,5 +1,74 @@
 package com.instagram.service;
 
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.qlrm.mapper.JpaResultMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.instagram.dto.SubscribeDto;
+import com.instagram.entity.Subscribe;
+import com.instagram.handler.CustomApiException;
+import com.instagram.repository.SubscribeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class SubscribeService {
 
+	private final SubscribeRepository subscribeRepository;
+	private final EntityManager em;
+	
+	@Transactional(readOnly = true)
+	public List<SubscribeDto> 구독리스트(int principalId, int pageUserId){
+		
+		// 쿼리 준비
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+		sb.append("if ((SELECT 1 FROM Subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, ");
+		sb.append("if ((?=u.id), 1, 0) equalUserState ");
+		sb.append("FROM User u INNER JOIN Subscribe s ");
+		sb.append("ON u.id = s.toUserId ");
+		sb.append("WHERE s.fromUserId = ?"); // 세미콜론 첨부하면 안됨
+		
+		// 1.물음표 principalId
+		// 2.물음표 principalId
+		// 3.물음표 pageUserId
+		
+		// 쿼리 완성
+		Query query = em.createNativeQuery(sb.toString())
+				.setParameter(1, principalId)
+				.setParameter(2, principalId)
+				.setParameter(3, principalId);
+				
+		
+		// 쿼리 실행 (qlrm 라이브러리 필요 = DTO에 DB결과를 매핑하기 위해서)
+		JpaResultMapper result = new JpaResultMapper();
+		
+		List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class);
+		
+		return subscribeDtos;
+	}
+	
+	
+	@Transactional
+	public void 구독하기(int fromUserId, int toUserId) {
+		try {
+			subscribeRepository.mSubscribe(fromUserId, toUserId);
+		}catch(Exception e) {
+			throw new CustomApiException("이미 구독을 하였습니다.");
+		}	
+	}
+	
+	@Transactional
+	public void 구독취소하기(int fromUserId, int toUserId) {
+		subscribeRepository.mSubscribe(fromUserId, toUserId);
+	}
+	
+	
 }
